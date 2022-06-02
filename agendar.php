3066d1ff94d13bfc0ced2faa684567be66840d1d
Alansmatias <alamm.hhh@gmail.com>
2022-06-02 13:06:34 -0300

    Correção erros no back-end

--- agendar.php
+++ agendar.php
@@ -6,26 +6,26 @@
     <title>Agendar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-<body>
-<nav class="navbar navbar-expand-lg bg-light">
+<body class="bg-dark">
+<nav class="navbar navbar-expand-lg bg-secondary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/BarbeariaNovaGeracao/agendar.php">BNG</a>
+    <a class="navbar-brand text-white" href="/BarbeariaNovaGeracao/agendar.php">BNG</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/BarbeariaNovaGeracao/">Inicio</a>
+          <a class="nav-link active text-white" aria-current="page" href="/BarbeariaNovaGeracao/">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Agenda</a>
+          <a class="nav-link text-white" href="#">Agenda</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-<div class="container">
+<div class="container text-white">
     <div class="row">
         <div class="col mt-5">
             <h1>Agendar</h1>
